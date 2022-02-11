41fbfc4caffca8414eb2335a5fbc468e0a514b6d
ShineThyuZan <shinethyuzan2010@gmail.com>
2022-02-11 16:18:56 +0630

    search and filtering

--- app/src/main/java/com/example/cocktailrecipe/activities/FullScreenImageActivity.kt
+++ app/src/main/java/com/example/cocktailrecipe/activities/FullScreenImageActivity.kt
@@ -25,8 +25,6 @@ class FullScreenImageActivity : BaseActivity() {
             return intent
         }
     }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_screen_image)
@@ -46,7 +44,7 @@ class FullScreenImageActivity : BaseActivity() {
             val adRequest = AdRequest.Builder().build()
             mAdView!!.loadAd(adRequest)
         }
-        
+
         /** download imageUrl to phone storage */
         val download = this.findViewById<ImageView>(R.id.ivDownload)
 
@@ -65,118 +63,6 @@ class FullScreenImageActivity : BaseActivity() {
             onBackPressed()
         }
     }
-
-
-//    private fun loadAd() {
-//        var adRequest = AdRequest.Builder().build()
-//
-//        InterstitialAd.load(
-//            this, AD_UNIT_ID, adRequest,
-//            object : InterstitialAdLoadCallback() {
-//                override fun onAdFailedToLoad(adError: LoadAdError) {
-//
-//                    mInterstitialAd = null
-//                    mAdIsLoading = false
-//                    val error = "domain: ${adError.domain}, code: ${adError.code}, " +
-//                            "message: ${adError.message}"
-//                    Toast.makeText(
-//                        this@FullScreenImageActivity,
-//                        "onAdFailedToLoad() with error $error",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//
-//                override fun onAdLoaded(interstitialAd: InterstitialAd) {
-//                    mInterstitialAd = interstitialAd
-//                    mAdIsLoading = false
-//
-//                    }
-//            }
-//        )
-//    }
-//
-//    private fun createTimer(milliseconds: Long) {
-//        mCountDownTimer?.cancel()
-//
-//        mCountDownTimer = object : CountDownTimer(milliseconds, 50) {
-//            override fun onTick(millisUntilFinished: Long) {
-//                mTimerMilliseconds = millisUntilFinished
-//              //  timer.text = "seconds remaining: ${ millisUntilFinished / 1000 + 1 }"
-//            }
-//
-//            override fun onFinish() {
-//                mGameIsInProgress = false
-////                timer.text = "done!"
-////                retry_button.visibility = View.VISIBLE
-//            }
-//        }
-//    }
-//
-//    // Show the ad if it's ready. Otherwise toast and restart the game.
-//    private fun showInterstitial() {
-//        if (mInterstitialAd != null) {
-//            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
-//                override fun onAdDismissedFullScreenContent() {
-//
-//                    // Don't forget to set the ad reference to null so you
-//                    // don't show the ad a second time.
-//                    mInterstitialAd = null
-//                    loadAd()
-//                }
-//
-//                override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
-//
-//                    // Don't forget to set the ad reference to null so you
-//                    // don't show the ad a second time.
-//                    mInterstitialAd = null
-//                }
-//
-//                override fun onAdShowedFullScreenContent() {
-//
-//                    // Called when ad is dismissed.
-//                }
-//            }
-//            mInterstitialAd?.show(this)
-//        } else {
-//            Toast.makeText(this, "Ad wasn't loaded.", Toast.LENGTH_SHORT).show()
-//            startGame()
-//        }
-//
-//    }
-//
-//    private fun startGame() {
-//        if (!mAdIsLoading && mInterstitialAd == null) {
-//            mAdIsLoading = true
-//            loadAd()
-//        }
-//
-//     //   retry_button.visibility = View.INVISIBLE
-//        resumeGame(GAME_LENGTH_MILLISECONDS)
-//    }
-//
-//    private fun resumeGame(milliseconds: Long) {
-//        // Create a new timer for the correct length and start it.
-//        mGameIsInProgress = true
-//        mTimerMilliseconds = milliseconds
-//        createTimer(milliseconds)
-//        mCountDownTimer?.start()
-//    }
-//
-//    // Resume the game if it's in progress.
-//    public override fun onResume() {
-//        super.onResume()
-//
-//        if (mGameIsInProgress) {
-//            resumeGame(mTimerMilliseconds)
-//        }
-//    }
-//
-//    // Cancel the timer if the game is paused.
-//    public override fun onPause() {
-//        mCountDownTimer?.cancel()
-//        super.onPause()
-//    }
-
 
     override fun onBackPressed() {
         super.onBackPressed()
--- app/src/main/java/com/example/cocktailrecipe/activities/MeMePhotoShowActivity.kt
+++ app/src/main/java/com/example/cocktailrecipe/activities/MeMePhotoShowActivity.kt
@@ -1,7 +1,17 @@
 package com.example.cocktailrecipe.activities
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
@@ -14,12 +24,15 @@ import com.example.cocktailrecipe.delegate.MeMePhotoItemDelegate
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import kotlin.system.exitProcess
 
 class MeMePhotoShowActivity : BaseActivity(), MeMePhotoItemDelegate {
     private lateinit var memePhotoListAdapter: MemePhotoListAdapter
     private lateinit var mViewModel: CocktailViewModel
-    private lateinit var mMemeListData: MutableList<MemeData>
-    private var mAdView : AdView? = null
+    private var mAdView: AdView? = null
+    private var doubleBackToExitPressedOnce = false
+    private lateinit var memeDataList : MutableList<MemeData>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +41,7 @@ class MeMePhotoShowActivity : BaseActivity(), MeMePhotoItemDelegate {
 
         MobileAds.initialize(this) {
             mAdView = findViewById(R.id.adView)
-           val adRequest = AdRequest.Builder().build()
+            val adRequest = AdRequest.Builder().build()
             mAdView!!.loadAd(adRequest)
         }
 
@@ -44,24 +57,92 @@ class MeMePhotoShowActivity : BaseActivity(), MeMePhotoItemDelegate {
         //Api Response catch
         observe()
 
+        this.findViewById<ImageView>(R.id.ivNav)
+            .setOnClickListener {
+                findViewById<DrawerLayout>(R.id.drawerLayout).openDrawer(GravityCompat.START)
+            }
+
+        this.findViewById<androidx.appcompat.widget.SearchView>(R.id.searchView).setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val result = memeDataList.filter { data ->
+                    data.name.contains(query!!)
+                }
+                memePhotoListAdapter.clearData()
+                memePhotoListAdapter.appendNewData(result as MutableList<MemeData>)
+
+                return true
+            }
+
+            override fun onQueryTextChange(inputSearchText: String?): Boolean {
+                if(inputSearchText.isNullOrBlank()){
+                    memePhotoListAdapter.setNewDataList(memeDataList)
+                }
+                return true
+            }
+        })
+        findViewById<RecyclerView>(R.id.rvMeMeList).apply {
+            adapter = memePhotoListAdapter
+            layoutManager = GridLayoutManager(context, 2)
+            setHasFixedSize(true)
+        }
+
+
     }
 
     private fun observe() {
         mViewModel.memeListResponse.observe(this, Observer {
+             memeDataList = it.data.memes as MutableList<MemeData>
             memePhotoListAdapter.setNewDataList(it.data.memes as MutableList<MemeData>)
-            this.findViewById<RecyclerView>(R.id.rvMeMeList).apply {
-                adapter = memePhotoListAdapter
-                layoutManager = GridLayoutManager(context, 2)
-                setHasFixedSize(true)
-            }
         })
         mViewModel.mErrorLD.observe(
             this, Observer {
                 Toast.makeText(this, "In error ", Toast.LENGTH_SHORT).show()
             }
         )
-
     }
+
+    override fun onBackPressed() {
+        if (this.findViewById<DrawerLayout>(R.id.drawerLayout).isDrawerOpen(GravityCompat.START)) {
+            this.findViewById<DrawerLayout>(R.id.drawerLayout).closeDrawer(GravityCompat.START)
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                finish()
+                moveTaskToBack(true)
+                exitProcess(-0)
+            }
+
+            this.doubleBackToExitPressedOnce = true
+            showBackConfirmDialog()
+            Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+        }
+    }
+
+    private fun showBackConfirmDialog() {
+        val exitBtn: Button
+        val cancelBtn: Button
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_exit_confirmation)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        /** animate dialog with window animation */
+        dialog.window!!.setWindowAnimations(R.style.AnimationForDialog)
+        dialog.setCanceledOnTouchOutside(false)
+        exitBtn = dialog.findViewById(R.id.btnExit) as Button
+        cancelBtn = dialog.findViewById(R.id.btnCancel) as Button
+
+        cancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+        exitBtn.setOnClickListener {
+            //todo delete dataStore userAccessToken key and LoginState
+            finish()
+        }
+        dialog.show()
+    }
+
 
     override fun onMeMeItemClicked(memeVos: MemeData) {
         startActivity(FullScreenImageActivity.newIntent(this, memeVos.url))
